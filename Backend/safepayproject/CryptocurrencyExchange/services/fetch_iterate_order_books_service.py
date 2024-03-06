6aaa0f7c6316bbcace5d762b0c2afbc31f3b7273
Amna Shafiq <142417695+Amna-Shafiq@users.noreply.github.com>
2024-03-06 11:59:18 +0500

    added bonus task(bitfinex exchange)

--- Backend/safepayproject/CryptocurrencyExchange/services/fetch_iterate_order_books_service.py
+++ Backend/safepayproject/CryptocurrencyExchange/services/fetch_iterate_order_books_service.py
@@ -3,6 +3,7 @@ from django.conf import settings
 
 BINANCE_URL = 'https://api.binance.com/api/v3/depth?symbol=BTCUSDT'
 COINBASE_URL = 'https://api-public.sandbox.exchange.coinbase.com/products/BTC-USD/book?level=3'
+BITFINEX_URL = 'https://api.bitfinex.com/v1/book/btcusd'
 
 class ExchangeAPIService:
     @staticmethod
@@ -17,6 +18,9 @@ class ExchangeAPIService:
 
     def get_coinbase_data(self):
         return self.get_exchange_data(COINBASE_URL)
+
+    def get_bitfinex_data(self):
+        return self.get_exchange_data(BITFINEX_URL)
 
     def find_lowest_price_binance(self,asks_data, target_amount):
         lowest_price = None
@@ -37,6 +41,19 @@ class ExchangeAPIService:
         for ask_price, ask_amount, _ in asks_data:
             ask_price = float(ask_price)
             ask_amount = float(ask_amount)
+
+            if ask_amount >= target_amount:
+                if lowest_price is None or ask_price < lowest_price:
+                    lowest_price = ask_price
+
+        return lowest_price
+
+    def find_lowest_price_bitfinex(self, asks_data, target_amount):
+        lowest_price = None
+
+        for ask in asks_data:
+            ask_price = float(ask.get("price", 0))
+            ask_amount = float(ask.get("amount", 0))
 
             if ask_amount >= target_amount:
                 if lowest_price is None or ask_price < lowest_price:
--- Backend/safepayproject/CryptocurrencyExchange/views.py
+++ Backend/safepayproject/CryptocurrencyExchange/views.py
@@ -18,20 +18,24 @@ def exchange_rate(request):
     try:
         binance_data = service.get_binance_data()
         coinbase_data = service.get_coinbase_data()
+        bitfinex_data = service.get_bitfinex_data()
     except ValueError as e:
         return Response({'error': str(e)}, status=500)
 
     binance_data_asks = binance_data.get("asks", [])
     coinbase_data_asks = coinbase_data.get("asks", [])
+    bitfinex_data_asks = bitfinex_data.get("asks", [])
 
     binance_price = service.find_lowest_price_binance(binance_data_asks, amount)
     coinbase_price = service.find_lowest_price_coinbase(coinbase_data_asks, amount)
+    bitfinex_price = service.find_lowest_price_bitfinex(bitfinex_data_asks, amount)
 
-    best_price = min(filter(None, [binance_price, coinbase_price]))
+    best_price = min(filter(None, [binance_price, coinbase_price, bitfinex_price]))
 
     response_data = {
         'binance_price': binance_price,
         'coinbase_price': coinbase_price,
+        'bitfinex_price': bitfinex_price,
         'best_price': best_price
     }
 
